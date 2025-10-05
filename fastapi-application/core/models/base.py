9631bd17bfd1280b883c114e71351ad9ab7bfd87
Alexander Karpov <alexanderak.0501@gmail.com>
2025-10-05 20:21:54 +0300

    fix the grammar error in name of base class

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -10,7 +10,7 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
-    def __table_name__(cls) -> str:
+    def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
