7ff1940eaae4801de963e314232eccf1a8a7d932
Alexander Karpov <alexanderak.0501@gmail.com>
2025-10-12 22:08:27 +0300

    change init in db_helper

--- fastapi-application/core/models/db_helper.py
+++ fastapi-application/core/models/db_helper.py
@@ -1,18 +1,22 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 from core.config import settings
 
-
 class DatabaseHelper:
     def __init__(
-            self,
-            url: str,
-            echo: bool = False,
-            echo_pool: bool = False,
-            pool_size: int = 5,
-            max_overflow: int = 10,
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
     ) -> None:
         self.engine: AsyncEngine = create_async_engine(
             url=url,
@@ -20,6 +24,7 @@ class DatabaseHelper:
             echo_pool=echo_pool,
             pool_size=pool_size,
             max_overflow=max_overflow,
+
         )
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
@@ -28,12 +33,12 @@ class DatabaseHelper:
             expire_on_commit=False,
         )
 
-        async def dispose(self) -> None:
-            await self.engine.dispose()
+    async def dispose(self) -> None:
+        await self.engine.dispose()
 
-        async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
-            async with self.session_factory() as session:
-                yield session
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
 
 db_helper = DatabaseHelper(
     url=str(settings.db.url),
